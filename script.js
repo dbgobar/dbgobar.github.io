587ea39ce98c046d3a052558f143d57bac8a15eb
Diana B. Gobar <dianabgobar@gmail.com>
2016-11-28 13:06:40 -0500

    style changes

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    $('#fullpage').fullpage({
+
+      //Navigation
+      menu: '#myMenu',
+      lockAnchors: false,
+      anchors:['welcome', 'about', 'projects', 'contact'],
+      navigation: false,
+      navigationPosition: 'right',
+      navigationTooltips: ['introSlide', 'LRN-Quiz', 'Dreamscape', 'FemiList', 'TrumpyCat', 'ComingSoon'],
+      showActiveTooltip: false,
+      slidesNavigation: true,
+      slidesNavPosition: 'bottom',
+
+
+      //background colors
+      sectionsColor: ['#66ccff', '#66ccff', '#cceeff', '#e6f7ff']
+    });
+
+    // icon hover effects
+    $(".art").hover(function(){
+      $("#altFaves").html(" art.");
+      $("#altFaves").toggle("slow");
+    });
+    $(".song").hover(function(){
+      $("#altFaves").html(" Singing classical opera..and really all things musical.");
+      $("#altFaves").toggle("slow");
+    });
+    $(".typography").hover(function(){
+      $("#altFaves").html(" Hand-lettering and Typography.");
+      $("#altFaves").toggle("slow");
+    });
+    $(".yoga").hover(function(){
+      $("#altFaves").html(" Yoga and meditation.");
+      $("#altFaves").toggle("slow");
+    });
+    $(".animals").hover(function(){
+      $("#altFaves").html("Animal rights and environmental causes.");
+      $("#altFaves").toggle("slow");
+    });
+    $(".book").hover(function(){
+      $("#altFaves").html(" Fairytale stories");
+      $("#altFaves").toggle("slow");
+    });
+    $(".chocolate").hover(function(){
+      $("#altFaves").html(" Chocolate, chocolate, chocolate!.");
+      $("#altFaves").toggle("slow");
+    });
+    $(".diy").hover(function(){
+      $("#altFaves").html(" Any and all DIY projects. I'm a handy person!");
+      $("#altFaves").toggle("slow");
+    });
+
+  });
